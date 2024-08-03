e62df80c7a9ad64e9c99c5e6c587817013ce57fd
acpucio <acpucio@gmail.com>
2024-08-03 03:14:23 +0000

    transactions added

--- database.py
+++ database.py
@@ -27,3 +27,26 @@ def load_job_from_db(id):
             return None
         else:
             return rows[0]._asdict()
+
+def add_application_to_db(job_id, application):
+    print("Inside add_application_to_db", application)
+    with engine.connect() as conn:
+        trans = conn.begin()
+        try:
+            query = text("""
+                INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) 
+                VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)
+                """)
+            conn.execute(query, {
+                        'job_id': job_id, 
+                        'full_name': application['full_name'], 
+                        'email': application['email'], 
+                        'linkedin_url': application['linkedin_url'], 
+                        'education': application['education'], 
+                        'work_experience': application['work_experience'], 
+                        'resume_url': application['resume_url']
+            })
+            trans.commit()
+        except Exception as e:
+            trans.rollback()
+            print(f"Error inserting data: {e}")

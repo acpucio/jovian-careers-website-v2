95bed097f6baec014dad5d2083f01bb90a9eb2e9
acpucio <acpucio@gmail.com>
2024-08-03 01:43:45 +0000

    added application form

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def hello_jovian():
@@ -24,8 +22,14 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   
-  return render_template('jobpage.html', 
-                         job=job)
+  return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods = ["post"] )
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
   
+  return render_template('application_submitted.html', application=data, job=job)
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
